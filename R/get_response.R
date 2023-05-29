181e843768e6b706e5c876eba27b6c75e51e0784
Callum Savage <callum.s.savage@gmail.com>
2023-05-29 18:02:02 +1000

    Remove max_tries args as I don't see myself needing them

--- R/get_response.R
+++ R/get_response.R
@@ -1,21 +1,33 @@
 #' Get a response from the BOM API
 #'
-#' @param format Response content format
-#' @param request Request type
-#' @param ... Optional key value pairs
-#' @param max_tries Maximum number of times to retry the request if an error is
-#'   returned
+#' Make an arbitrary request to the API, and receive the response object. This
+#' function is intentionally permissive, meaning that it will allow you to make
+#' any request that you like (so long as you specify format and request). No
+#' error checking is perfomed on the inputs.
 #'
-#' @return A response object
+#' @param format Response content format
+#' @param request A character string defining the request type to pass on to the
+#'   KISTERS API. Use [list_requests()] to see available requests.
+#' @param ... Optional key value pairs which can be used to narrow down the
+#'   request. Vectors of length greater than one will be collapsed into a comma
+#'   separated list. It is possible to use an asterix character (*) as wildcard
+#'   in some fields, however this is not recommended as the data naming
+#'   conventions can be inconsistent. Use [list_query_fields()] to see available
+#'   options.
+#'
+#' @returns A response object.
+#' @seealso [get_bom_data()] for a simplified API interface which is suitable
+#'   for most requests returning rectangular data.
 #' @export
 #'
 #' @examples
+#' # Get a geojson file of discharge stations
 #' get_bom_response(
-#'   format = "json",
+#'   format = "geojson",
 #'   request = "getStationList",
 #'   parametertype_name = "Water Course Discharge"
 #' )
-get_bom_response <- function(format, request, ..., max_tries = 1) {
+get_bom_response <- function(format, request, ...) {
   bom_url <- "http://www.bom.gov.au/waterdata/services"
   query <- list(
     service = "kisters",
@@ -27,16 +39,17 @@ get_bom_response <- function(format, request, ..., max_tries = 1) {
   req <- httr2::request(bom_url)
   req <- httr2::req_url_query(req, !!!query)
   req <- httr2::req_error(req, body = body_error)
-  req <- httr2::req_retry(req, max_tries = max_tries)
   httr2::req_perform(req)
 }
 
 #' Get BOM data
 #'
-#' @param request Desired request e.g. "getStationList"
-#' @param ... Optional key value pairs, passed on to `get_bom_response`
+#' @inheritParams get_bom_response
+#' @param ... Optional key value pairs passed on to [get_bom_response()].
 #'
-#' @return A tibble
+#' @seealso [get_bom_response()] for more fine-grained control over API
+#'   requests.
+#' @returns A tibble.
 #' @export
 #'
 #' @examples
@@ -45,8 +58,7 @@ get_bom_data <- function(request, ...) {
   resp <- get_bom_response(
     format = "csv",
     request = request,
-    ...,
-    max_tries = 1
+    ...
   )
   resp_body <- httr2::resp_body_string(resp)
   bom_col_types <- readr::cols(
