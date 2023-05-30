4a961ce024c5684973f35e8b2844153ad0a60d98
Callum Savage <callum.s.savage@gmail.com>
2023-05-30 12:32:20 +1000

    Update docs in get_response

--- R/get_response.R
+++ R/get_response.R
@@ -1,23 +1,26 @@
 #' Get a response from the BOM API
 #'
-#' Make an arbitrary request to the API, and receive the response object. This
-#' function is intentionally permissive, meaning that it will allow you to make
-#' any request that you like (so long as you specify format and request). No
-#' error checking is perfomed on the inputs.
+#' `get_bom_response` takes an arbitrary API request and returns the response
+#' object. Inputs are not error checked, allowing you to make requests which
+#' fall outside the scope of [get_bom_data()]. This can be useful if you want to
+#' return an unconventional format (e.g. geojson), want make an uncommon
+#' request, or just want to interface directly with the API.
 #'
-#' @param format Response content format
+#' @param format A string giving the format of the response content. Valid
+#'   options will depend on the request. Use [list_formats()] to see available
+#'   options.
 #' @param request A character string defining the request type to pass on to the
-#'   KISTERS API. Use [list_requests()] to see available requests.
-#' @param ... Optional key value pairs which can be used to narrow down the
+#'   BOM API. Use [list_requests()] to see available options.
+#' @param ... Optional named query fields which can be used to narrow the
 #'   request. Vectors of length greater than one will be collapsed into a comma
 #'   separated list. It is possible to use an asterix character (*) as wildcard
-#'   in some fields, however this is not recommended as the data naming
-#'   conventions can be inconsistent. Use [list_query_fields()] to see available
-#'   options.
+#'   in some fields, however this is not recommended as the naming conventions
+#'   can be inconsistent. Use [list_query_fields()] to see available options,
+#'   including fields which accept a comma separated list or wildcard.
 #'
 #' @returns A response object.
-#' @seealso [get_bom_data()] for a simplified API interface which is suitable
-#'   for most requests returning rectangular data.
+#' @seealso [get_bom_data()] for a simplified API interface suitable for
+#'   requests that return rectangular data.
 #' @export
 #'
 #' @examples
@@ -33,11 +36,12 @@ get_bom_response <- function(format, request, ...) {
     service = "kisters",
     type = "QueryServices",
     format = format,
-    request = request,
-    ...
+    request = request
   )
+  # Collapse query fields into a comma separated list
+  query_fields <- map(list(...), stringr::str_flatten_comma)
   req <- httr2::request(bom_url)
-  req <- httr2::req_url_query(req, !!!query)
+  req <- httr2::req_url_query(req, !!!c(query, query_fields))
   req <- httr2::req_error(req, body = body_error)
   httr2::req_perform(req)
 }
@@ -45,7 +49,6 @@ get_bom_response <- function(format, request, ...) {
 #' Get BOM data
 #'
 #' @inheritParams get_bom_response
-#' @param ... Optional key value pairs passed on to [get_bom_response()].
 #'
 #' @seealso [get_bom_response()] for more fine-grained control over API
 #'   requests.
@@ -61,16 +64,20 @@ get_bom_data <- function(request, ...) {
     ...
   )
   resp_body <- httr2::resp_body_string(resp)
-  bom_col_types <- readr::cols(
-    station_id = readr::col_integer(),
-    station_latitude = readr::col_double(),
-    station_longitude = readr::col_double(),
-    .default = readr::col_character()
-  )
-  readr::read_delim(
+  bom_data <- readr::read_delim(
     resp_body,
     delim = ";",
-    col_types = bom_col_types
+    col_types = readr::cols(.default = readr::col_character()),
+    na = "",
+    progress = FALSE
+  )
+  # Limited type conversion
+  numeric_cols <- c("station_latitude", "station_longitude")
+  integer_cols <- c("station_id")
+  dplyr::mutate(
+    bom_data,
+    dplyr::across(dplyr::any_of(numeric_cols), as.numeric),
+    dplyr::across(dplyr::any_of(integer_cols), as.integer)
   )
 }
 
