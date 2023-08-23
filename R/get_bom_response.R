7b09c0844d5a2c49face342eaf6ee800e7846bf1
Callum Savage <callum.s.savage@gmail.com>
2023-08-23 12:31:44 +1000

    Reorganise functions more logically

--- R/get_bom_response.R
+++ R/get_bom_response.R
@@ -1,3 +1,95 @@
+#' Get BOM data
+#'
+#' @inheritParams get_bom_response
+#' @param request A character string defining the request type to pass on to the
+#'   BOM API. Must be one of: `"getStationList"`, `"getParameterList"`, or
+#'   `"getTimeseriesList"`.
+#' @param returnfields A character vector of columns to include in the returned
+#'   tibble. If not defined, the default columns will be determined by the API.
+#'   Use [list_return_fields()] to see available options.
+#' @seealso [get_bom_response()] for more fine-grained control over API
+#'   requests.
+#' @return A tibble with columns matching `returnfields`.
+#' @export
+#'
+#' @examples
+#' get_bom_data(
+#'   request = "getStationList",
+#'   parametertype_name = "Water Course Level"
+#' )
+get_bom_data <- function(request, ...) {
+  # List and timeseries responses are unpacked differently
+  if (request %in% c("getStationList", "getParameterList", "getTimeseriesList")) {
+    bom_data <- get_bom_list(request = request, ...)
+  } else if (request == "getTimeseriesValues") {
+    bom_data <- get_bom_timeseries(request = request, ...)
+  } else
+    # TODO polish this error message
+    # TODO add info on provided request, and what would be valid
+    cli::cli_abort(c("Invalid request."))
+  # TODO move type conversion to separate function
+  # Perform limited type conversion
+  numeric_cols <- c("station_latitude", "station_longitude")
+  integer_cols <- c("station_id")
+  timestamp_cols <- c("Timestamp", "from", "to")
+  dplyr::mutate(
+    bom_data,
+    dplyr::across(dplyr::any_of(numeric_cols), as.numeric),
+    dplyr::across(dplyr::any_of(integer_cols), as.integer),
+    dplyr::across(dplyr::any_of(timestamp_cols), lubridate::as_datetime)
+  )
+  # TODO make a function for name cleaning
+}
+
+get_bom_list <- function(request, returnfields = NULL, ...) {
+  # Get response in csv format
+  resp <- get_bom_response(
+    format = "csv",
+    request = request,
+    returnfields = returnfields,
+    ...
+  )
+  # Read csv from response body
+  resp_body <- httr2::resp_body_string(resp)
+  readr::read_delim(
+    resp_body,
+    delim = ";",
+    col_types = readr::cols(.default = readr::col_character()),
+    na = "",
+    progress = FALSE
+  )
+}
+
+# TODO warn if zero rows returned for a given station
+# TODO keep empty rows when unnesting
+get_bom_timeseries <- function(request, returnfields = NULL, md_returnfields = NULL, ...) {
+  resp <- get_bom_response(
+    format = "json",
+    request = request,
+    ...,
+    returnfields = returnfields
+  )
+  resp_body <- httr2::resp_body_json(resp)
+  # Unpack json response into a nested tibble with one row per timeseries
+  ts1 <- tibble::tibble("resp_body" = resp_body)
+  # Expand resp_body into columns (timeseries is still nested)
+  ts2 <- tidyr::unnest_wider(ts1, col = "resp_body")
+  # Select columns which will be returned
+  # The timeseries is returned in a column called "data"
+  ts3 <- dplyr::select(ts2, dplyr::any_of(c(md_returnfields, "data")))
+  # Unnest timeseries from "data" column
+  ts4 <- tidyr::unnest_longer(ts3, col = "data")
+  ts5 <- tidyr::unnest_wider(ts4, col = "data", names_sep = "_")
+  # The timeseries' column names were returned as metadata
+  # Extract the correct timeseries names
+  ts_names <- unlist(stringr::str_split(ts2$columns[[1]], ","))
+  # Identify the temporary timeseries names (of the form data_1, data_2, etc.)
+  ts_oldnames <- stringr::str_subset(names(ts5), "^data_\\d+$")
+  # Replace old names with the new
+  # TODO check that length(ts_names) == length(ts_oldnames)
+  dplyr::rename_with(ts5, .fn = \(x) ts_names, .cols = dplyr::all_of(ts_oldnames))
+}
+
 #' Get a response from the BOM API
 #'
 #' `get_bom_response` takes an arbitrary API request and returns the response
@@ -64,98 +156,4 @@ extract_body_error <- function(resp) {
   } else {
     stringr::str_glue("Body has unknown content type: {content_type}")
   }
-}
-
-# TODO remove explicit returnfields arg
-#' Get BOM data
-#'
-#' @inheritParams get_bom_response
-#' @param request A character string defining the request type to pass on to the
-#'   BOM API. Must be one of: `"getStationList"`, `"getParameterList"`, or
-#'   `"getTimeseriesList"`.
-#' @param returnfields A character vector of columns to include in the returned
-#'   tibble. If not defined, the default columns will be determined by the API.
-#'   Use [list_return_fields()] to see available options.
-#' @seealso [get_bom_response()] for more fine-grained control over API
-#'   requests.
-#' @return A tibble with columns matching `returnfields`.
-#' @export
-#'
-#' @examples
-#' get_bom_data(
-#'   request = "getStationList",
-#'   parametertype_name = "Water Course Level"
-#' )
-get_bom_data <- function(request, ...) {
-  # List and timeseries responses are unpacked differently
-  if (request %in% c("getStationList", "getParameterList", "getTimeseriesList")) {
-    bom_data <- get_bom_list(request = request, ...)
-  } else if (request == "getTimeseriesValues") {
-    bom_data <- get_bom_timeseries(request = request, ...)
-  } else
-    # TODO polish this error message
-    # TODO add info on provided request, and what would be valid
-    cli::cli_abort(c("Invalid request."))
-  # TODO move type conversion to separate function
-  # Perform limited type conversion
-  numeric_cols <- c("station_latitude", "station_longitude")
-  integer_cols <- c("station_id")
-  timestamp_cols <- c("Timestamp", "from", "to")
-  dplyr::mutate(
-    bom_data,
-    dplyr::across(dplyr::any_of(numeric_cols), as.numeric),
-    dplyr::across(dplyr::any_of(integer_cols), as.integer),
-    dplyr::across(dplyr::any_of(timestamp_cols), lubridate::as_datetime)
-  )
-  # TODO make a function for name cleaning
-}
-
-get_bom_list <- function(request, returnfields = NULL, ...) {
-  # Get response in csv format
-  resp <- get_bom_response(
-    format = "csv",
-    request = request,
-    returnfields = returnfields,
-    ...
-  )
-  # Read csv from response body
-  resp_body <- httr2::resp_body_string(resp)
-  readr::read_delim(
-    resp_body,
-    delim = ";",
-    col_types = readr::cols(.default = readr::col_character()),
-    na = "",
-    progress = FALSE
-  )
-}
-
-# TODO warn if zero rows returned for a given station
-# TODO md_returnfields should be an explicit arg
-# TODO keep empty rows when unnesting
-get_bom_timeseries <- function(request, returnfields = NULL, md_returnfields = NULL, ...) {
-  resp <- get_bom_response(
-    format = "json",
-    request = request,
-    ...,
-    returnfields = returnfields
-  )
-  resp_body <- httr2::resp_body_json(resp)
-  # Unpack json response into a nested tibble with one row per timeseries
-  ts1 <- tibble::tibble("resp_body" = resp_body)
-  # Expand resp_body into columns (timeseries is still nested)
-  ts2 <- tidyr::unnest_wider(ts1, col = "resp_body")
-  # Select columns which will be returned
-  # The timeseries is returned in a column called "data"
-  ts3 <- dplyr::select(ts2, dplyr::any_of(c(md_returnfields, "data")))
-  # Unnest timeseries from "data" column
-  ts4 <- tidyr::unnest_longer(ts3, col = "data")
-  ts5 <- tidyr::unnest_wider(ts4, col = "data", names_sep = "_")
-  # The timeseries' column names were returned as metadata
-  # Extract the correct timeseries names
-  ts_names <- unlist(stringr::str_split(ts2$columns[[1]], ","))
-  # Identify the temporary timeseries names (of the form data_1, data_2, etc.)
-  ts_oldnames <- stringr::str_subset(names(ts5), "^data_\\d+$")
-  # Replace old names with the new
-  # TODO check that length(ts_names) == length(ts_oldnames)
-  dplyr::rename_with(ts5, .fn = \(x) ts_names, .cols = dplyr::all_of(ts_oldnames))
 }
