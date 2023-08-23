e877c41f1835a5dcd03ced5ca19888ecc3b31aa6
Callum Savage <callum.s.savage@gmail.com>
2023-08-23 12:19:28 +1000

    Write additional tests for get_bom_data

--- tests/testthat/test-get_bom_data.R
+++ tests/testthat/test-get_bom_data.R
@@ -7,15 +7,36 @@
 # Invalid format
 # Invalid tries
 
-test_that("get_bom_response returns a valid response", {
-  response <- get_bom_response(
+test_that("get_bom_response works with manditory arguments", {
+  r <- get_bom_response(
+    format = "json",
+    request = "getParameterTypeList"
+  )
+  expect_equal(httr2::resp_status(r), 200)
+  expect_equal(httr2::resp_content_type(r), "application/json")
+  expect_true(length(httr2::resp_body_json(r)) > 140)
+})
+
+test_that("get_bom_response works with atomic arguments", {
+  r <- get_bom_response(
     format = "csv",
     request = "getStationList",
     station_no = "G0060047"
   )
-  expect_true(length(httr2::resp_headers(response)) > 0)
-  expect_true(httr2::resp_status(response) == 200)
-  expect_true(httr2::resp_content_type(response) == "application/csv")
+  expect_equal(httr2::resp_status(r), 200)
+  expect_equal(httr2::resp_content_type(r), "application/csv")
+  expect_true(length(httr2::resp_headers(r)) > 0)
+})
+
+test_that("get_bom_response works with vector arguments", {
+  r <- get_bom_response(
+    format = "tabjson",
+    request = "getParameterList",
+    station_no = c("1200.1", "1203.1", "1204.1")
+  )
+  expect_equal(httr2::resp_status(r), 200)
+  expect_equal(httr2::resp_content_type(r), "application/json")
+  expect_equal(httr2::resp_body_json(r)[[2]][[5]], "Water Course Discharge")
 })
 
 test_that("get_bom_response errors if arguments are invalid", {
@@ -24,6 +45,9 @@ test_that("get_bom_response errors if arguments are invalid", {
   expect_error(get_bom_response(format = "", request = ""))
   expect_error(get_bom_response(format = "json"))
   expect_error(get_bom_response(request = "getStationList"))
+})
+
+test_that("get_bom_response returns error messages from the response body", {
 })
 
 test_that("get_bom_data always returns a tibble", {
@@ -36,3 +60,31 @@ test_that("get_bom_data always returns a tibble", {
   expect_true(tibble::is_tibble(data2))
   expect_true(nrow(data2) == 0)
 })
+
+test_that("get_bom_data works for all supported request types", {
+  station_list <- get_bom_data(
+    request = "getStationList",
+    station_no = c(403239, 403251)
+  )
+  parameter_list <- get_bom_data(
+    request = "getParameterList",
+    station_no = c(403239, 403251)
+  )
+  timeseries_list <- get_bom_data(
+    request = "getTimeseriesList",
+    station_no = c(403239, 403251)
+  )
+  timeseries_values <- get_bom_data(
+    request = "getTimeseriesValues",
+    ts_id = c(254923010, 255253010),
+    from = "2020-01-01",
+    to = "2020-01-02",
+    md_returnfields = c("ts_id", "station_no"),
+    returnfields = c("Timestamp", "Value", "Quality Code", "Interpolation Type")
+  )
+  expect_equal(dim(station_list), c(2, 5))
+  expect_equal(dim(parameter_list), c(4, 6))
+  expect_equal(dim(timeseries_list), c(100, 7))
+  expect_equal(dim(timeseries_values), c(2, 5))
+})
+
