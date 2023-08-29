2ab35df90d4b1bc763eb1a1fe02b9dd97a8e20b2
Callum Savage <callum.s.savage@gmail.com>
2023-08-29 15:13:14 +1000

    Draft additional timeseries tests

--- tests/testthat/test-wrappers.R
+++ tests/testthat/test-wrappers.R
@@ -16,6 +16,7 @@ test_that("get_wdo_list only accepts list requests", {
 test_that("column type conversions are applied", {
   # TODO cover all type conversions
   # TODO look at list and timeseries requests
+  # TODO check that everything else is a character
 
   df <- get_wdo_list(
     request = "getParameterList",
@@ -35,6 +36,29 @@ test_that("timeseries values are always returned as a tibble", {
                               to = "2021-01-07")
   expect_true(tibble::is_tibble(ts))
   expect_equal(dim(ts), c(1008, 6))
+})
+
+test_that("the return fields are idential for all timeseries", {
+  # Don't know how to phrase, but I'm considering the case where different
+  # timeseries might have different return fields (e.g. because of optional args)
+
+  ts <- get_timeseries_values(ts_id = c(608153010, 425964010),
+                              from = "2021-01-01",
+                              to = "2021-01-02")
+
+
+})
+
+test_that("metadata is identical for all timeseries", {
+
+})
+
+test_that("null values are converted to NAs in timeseries", {
+  # This has introduced errors before
+})
+
+test_that("A tibble is returned even when metadata isn't specified", {
+  # Include invalid metadata
 })
 
 test_that("empty timeseries metadata is maintained in timeseries tibble", {
