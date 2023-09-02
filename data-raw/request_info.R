b987c6ead03229cda7a25ee6490b42465d0e7eb3
Callum Savage <callum.s.savage@gmail.com>
2023-09-03 09:57:46 +1000

    Update wdo_formats with content type

--- data-raw/request_info.R
+++ data-raw/request_info.R
@@ -1,16 +1,20 @@
-# Prepare tables describing query options for the Water Data Online API
+# This script imports the query specification for the Water Data Online API and
+# simplifies it into a set of tables. The tables are saved as R/sysdata.rda and
+# are available internally, but are not exported.
 
 library(dplyr)
 library(tidyr)
 
-# Get request information from the API
+# Get request info --------------------------------------------------------
+
+# Request query info from the API
 
 request_info_resp <- get_wdo_response(
   format = "json",
   request = "getRequestInfo"
 )
 
-# Unpack json list into a nested tibble
+# Unpack the returned json response into a nested tibble
 
 request_info <- request_info_resp |>
   httr2::resp_body_json() |>
@@ -20,13 +24,19 @@ request_info <- request_info_resp |>
   unnest_longer(Requests) |>
   unnest_wider(Requests)
 
-# Extract request descriptions
+# Extract tables of query options -----------------------------------------
+
+# Get descriptions for each request type
 
 wdo_requests <- request_info |>
   select(Request, Description, Subdescription) |>
   janitor::clean_names()
 
-# Define a helper function for unpacking each column into a separate tibble
+# Other query options are nested more deply, and can be extracted using a helper
+# function.
+#
+# `unpack_col` unpacks each nested column into a tidy tibble, and renames the
+# column with a provided name
 
 unpack_col <- function(request_info, col, newname) {
   request_info |>
@@ -41,7 +51,8 @@ unpack_col <- function(request_info, col, newname) {
     rename("{{ newname }}" := name)
 }
 
-# Unpack columns into new tibbles, keeping request as a key
+# Unpack columns into new tibbles, keeping request as a key. Some columns
+# require a little extra processing
 
 wdo_formats <- request_info |>
   unpack_col(Formats, format)
@@ -68,6 +79,57 @@ wdo_date_formats <- request_info |>
 
 wdo_return_fields <- request_info |>
   unpack_col(Returnfields, return_field)
+
+# Identify content types --------------------------------------------------
+
+# Each format has a returned content type which I need to know to process the
+# response objects
+
+# Select one representative request type per format
+
+format_requests <- wdo_formats |>
+  add_count(request) |>
+  slice_max(n, by = format, with_ties = FALSE)
+
+# Define test parameters
+
+station_no <- "419093"
+ts_id <- "343344010"
+from <- "2020-01-01"
+to <- "2020-01-02"
+catchment_id <- 0
+
+# Make a small request for each format and identify the content type. Only four
+# requests are needed to cover all content types
+
+get_content_type <- function(request, format) {
+  if (request == "getStationList") {
+    resp <- get_wdo_response(format, request, station_no = station_no)
+  } else if (request == "getTimeseriesValues") {
+    resp <- get_wdo_response(format, request, ts_id = ts_id)
+  } else if (request == "getGraph") {
+    resp <- get_wdo_response(format, request, ts_id = ts_id, from = from, to = to)
+  } else if (request == "getCatchmentHierarchy") {
+    resp <- get_wdo_response(format, request, catchment_id = 0)
+  } else {
+    return("Unknown")
+  }
+  httr2::resp_content_type(resp)
+}
+
+content_types <- format_requests |>
+  rowwise() |>
+  mutate(content_type = get_content_type(request, format)) |>
+  distinct(format, content_type)
+
+# Attach format types to wdo_formats
+
+wdo_formats <- left_join(wdo_formats, content_types, join_by("format"))
+
+# TODO identify the default format for each request
+# TODO identify the default return fields for each request
+# this probably only needs to be done for the core requests, or I could
+# rewrite the above somehow
 
 # Save data internally (not exported)
 
